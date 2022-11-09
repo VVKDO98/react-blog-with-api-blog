dec32e61370faaf398899e276143b226f2bf66b5
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-09 16:24:26 +0100

    Base menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,31 +1,34 @@
 import React from 'react';
+import { IoSearch, IoCreate } from "react-icons/io5";
 
 const Menu = () => {
     return (
-        <nav>
-            <div>
-                <ul>
-                    <li>Homepage</li>
-                    <li>About</li>
-                    <li>Categories</li>
-                    <li>Pages</li>
-                </ul>
-            </div>
-            <div>
-                <img src="./assets/img/logo.png" alt="Logo NoteBook" />
-            </div>
-            <div>
-                <ul>
-                    <li><img src="./assets/img/search.png" alt="" /></li>
-                    <li><img src="./assets/img/create.png" alt="" /></li>
-                    <li>Contact</li>
-                    <li>
-                        <select name="langue">
-                            <option value="en">En</option>
-                            <option value="fr">Fr</option>
-                        </select>
-                    </li>
-                </ul>
+        <nav className='w-full h-20 bg-lightgreen flex items-center'>
+            <div id='navContainer' className='w-4/5 m-auto flex justify-between items-center'>
+                <div className='hidden lg:block'>
+                    <ul className='flex items-center gap-14'>
+                        <li><a href="/" className='font-medium text-sm hover:text-green'>Homepage</a></li>
+                        <li><a href="/" className='font-medium text-sm hover:text-green'>About</a></li>
+                        <li><a href="/" className='font-medium text-sm hover:text-green'>Categories</a></li>
+                        <li><a href="/" className='font-medium text-sm hover:text-green'>Pages</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <a href="/"><img src="./assets/img/logo.png" alt="Logo NoteBook" /></a>
+                </div>
+                <div className='hidden lg:block'>
+                    <ul className='flex items-center gap-14'>
+                        <li><a href="/"><IoSearch className="text-lg hover:text-green" /></a></li>
+                        <li><a href="/"><IoCreate className="text-lg hover:text-green" /></a></li>
+                        <li><a href="/" className='font-medium text-sm hover:text-green'>Contact</a></li>
+                        <li>
+                            <select name="langue">
+                                <option value="en">En</option>
+                                <option value="fr">Fr</option>
+                            </select>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
