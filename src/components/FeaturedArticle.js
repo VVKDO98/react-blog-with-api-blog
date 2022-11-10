9f0deb10f8b003bc59c5db185009a4883f5116a5
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-10 11:59:07 +0100

    Changing Fetures This Month img

--- src/components/FeaturedArticle.js
+++ src/components/FeaturedArticle.js
@@ -7,7 +7,9 @@ const FeaturedArticle = ({data}) => {
         <div className='mt-10 w-full lg:w-1/2'>
             <a href="/" className='text-xs text-white bg-green rounded px-2 py-0.5'>{data.categories.name}</a>
             <h1 className='text-xl font-semibold leading-7 mt-4'>{data.title}</h1>
-            <img className='mt-6 rounded-lg' src={"./assets/img/" + data.image_url} alt={data.tite} />
+            <div className='w-full h-80 mt-6 rounded-lg bg-lightgreen flex justify-center items-center overflow-hidden'>
+                <img src={"./assets/img/" + data.image_url} alt={data.tite} />
+            </div>
             <div className='my-6 flex flex-row items-center gap-3 text-base font-normal text-grey'>
                 <span>{data.user.name}</span>
                 <span>|</span>
