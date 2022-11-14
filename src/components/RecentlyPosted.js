da36b6bb975fad100abcbaf2940a5f1576fba598
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-14 11:00:17 +0100

    Add style to RecentlyPosted

--- src/components/RecentlyPosted.js
+++ src/components/RecentlyPosted.js
@@ -3,8 +3,8 @@ import RecentlyPostedArticle from './RecentlyPostedArticle';
 
 const RecentlyPosts = ({data}) => {
     return (
-        <div>
-            <h2>Recently Posted</h2>
+        <div className='w-11/12 m-auto lg:w-4/6 lg:m-0'>
+            <h3 className='font-semibold text-xl'><span className='py-0.5 px-2 text-white bg-green'>Recently</span> Posted</h3>
             {data.map((data, index) => <RecentlyPostedArticle data={data} key={index}/>)}
         </div>
     );
--- src/components/RecentlyPostedArticle.js
+++ src/components/RecentlyPostedArticle.js
@@ -3,15 +3,19 @@ import moment from 'moment/moment';
 
 const RecentlyPostedArticle = ({data}) => {
     return (
-        <div id='recentArticle'>
-            <img src={"./assets/img/" + data.image_url} alt={"Banner of " + data.title} />
-            <p>{data.categories.name}</p>
-            <h3>{data.title}</h3>
-            <div>
-                <p>{data.user.name}</p>
-                <p>{moment(data.created_at).format('DD MMMM YYYY')}</p>
+        <div id='recentArticle' className='w-full flex flex-col'>
+            <div className='mt-2 w-full h-52 rounded-lg flex justify-center items-center overflow-hidden lg:h-80'>
+                <img className='min-w-full min-h-full object-cover' src={"./assets/img/" + data.image_url} alt={"Banner of " + data.title} />
             </div>
-            <p>{data.excerpt}</p>
+            <div>
+                <p>{data.categories.name}</p>
+                <h3>{data.title}</h3>
+                <div>
+                    <p>{data.user.name}</p>
+                    <p>{moment(data.created_at).format('DD MMMM YYYY')}</p>
+                </div>
+                <p>{data.excerpt}</p>
+            </div>
         </div>
     );
 };
