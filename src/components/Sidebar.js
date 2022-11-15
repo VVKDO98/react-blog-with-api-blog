45fa089d8b689cfaa38580f53520def61dba9728
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-15 12:00:37 +0100

    SideAd Done

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import SideTopAuthors from './SideTopAuthors';
-import SideCategories from './SideCategories';
-import SideTodaysUpdate from './SideTodaysUpdate';
+// import SideCategories from './SideCategories';
+// import SideTodaysUpdate from './SideTodaysUpdate';
+import SideAd from './SideAd';
 
 const Sidebar = () => {
     return (
-        <div className='w-11/12 lg:w-2/6 lg:m-0'>
+        <div className='w-full lg:w-2/6 lg:m-0'>
             <SideTopAuthors/>
+            <SideAd/>
             {/* <SideCategories/>
             <SideTodaysUpdate/> */}
-            {/* <SideAd/>*/}
+            
         </div>
     );
 };
