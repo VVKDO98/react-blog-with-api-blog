f0a84ceb4863ba09f1c1c7289bdd5accba638702
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-16 11:38:05 +0100

    Text center

--- src/components/SideTodaysUpdate.js
+++ src/components/SideTodaysUpdate.js
@@ -8,19 +8,19 @@ const SideTodaysUpdate = () => {
             <div className='mt-10 w-full flex flex-row flex-wrap gap-4 lg:gap-6'>
                 <div className='w-[47%] h-28 bg-bannercolor flex flex-col justify-center items-center rounded-2xl lg:w-[44%]'>
                     <p className='text-2xl text-green font-semibold'>14</p>
-                    <p className='text-base text-black font-normal'>New Posts</p>
+                    <p className='text-base text-black font-normal text-center'>New Posts</p>
                 </div>
                 <div className='w-[47%] h-28 bg-bannercolor flex flex-col justify-center items-center rounded-2xl lg:w-[44%]'>
                     <p className='text-2xl text-green font-semibold'>480</p>
-                    <p className='text-base text-black font-normal'>Total Visitors</p>
+                    <p className='text-base text-black font-normal text-center'>Total Visitors</p>
                 </div>
                 <div className='w-[47%] h-28 bg-bannercolor flex flex-col justify-center items-center rounded-2xl lg:w-[44%]'>
                     <p className='text-2xl text-green font-semibold'>29</p>
-                    <p className='text-base text-black font-normal'>New Subscribers</p>
+                    <p className='text-base text-black font-normal text-center'>New Subscribers</p>
                 </div>
                 <div className='w-[47%] h-28 bg-bannercolor flex flex-col justify-center items-center rounded-2xl lg:w-[44%]'>
                     <p className='text-2xl text-green font-semibold'>138</p>
-                    <p className='text-base text-black font-normal'>Blog Read</p>
+                    <p className='text-base text-black font-normal text-center'>Blog Read</p>
                 </div>
             </div>
         </div>
