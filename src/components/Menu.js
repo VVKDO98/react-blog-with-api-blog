735b710ecb9ee43c2df3185a91a3050392a5cf1e
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-16 14:50:12 +0100

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,10 +8,18 @@ const Menu = () => {
                 <div id='navContainer' className='w-11/12 lg:w-4/5 m-auto flex justify-between items-center'>
                     <div className='hidden lg:block'>
                         <ul className='flex items-center lg:gap-4'>
-                            <li><a href="/" className='font-medium text-sm hover:text-green'>Homepage</a></li>
+                            <li>
+                                <select name="categories" className='font-medium text-sm bg-lightgreen'>
+                                    <option value="" className='font-medium text-sm'>Categories</option>
+                                    <option value="travel" className='font-medium text-sm'>Travel</option>
+                                    <option value="technology" className='font-medium text-sm'>Technology</option>
+                                    <option value="lifestyle" className='font-medium text-sm'>Lifestyle</option>
+                                    <option value="food" className='font-medium text-sm'>Food</option>
+                                    <option value="healthcare" className='font-medium text-sm'>Healthcare</option>
+                                </select>
+                            </li>
                             <li><a href="/" className='font-medium text-sm hover:text-green'>About</a></li>
-                            <li><a href="/" className='font-medium text-sm hover:text-green'>Categories</a></li>
-                            <li><a href="/" className='font-medium text-sm hover:text-green'>Pages</a></li>
+                            <li><a href="/" className='font-medium text-sm hover:text-green'>Contact</a></li>
                         </ul>
                     </div>
                     <div>
@@ -21,7 +29,6 @@ const Menu = () => {
                         <ul className='flex items-center lg:gap-4'>
                             <li><a href="/"><IoSearch className="text-lg hover:text-green" /></a></li>
                             <li><a href="/"><IoCreate className="text-lg hover:text-green" /></a></li>
-                            <li><a href="/" className='font-medium text-sm hover:text-green'>Contact</a></li>
                             <li>
                                 <select name="langue" className='font-medium text-sm bg-lightgreen'>
                                     <option value="en" className='font-medium text-sm'>En</option>
