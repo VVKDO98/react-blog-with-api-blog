9ab043bbd0a2661601c83712071e5783abe14c99
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-16 12:01:31 +0100

    Add a loading animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import FetchPosts from "../hooks/FetchPosts";
 const Home = () => {
     const {loading, error, data} = FetchPosts();
     if (error) return <p>Loading of articles</p>;
-    if (loading) return <p>Loading of articles</p>;
+    if (loading) return <div className="w-full h-screen flex items-center justify-center"><img src="./assets/img/spinner.svg" alt="" /></div>;
     
     console.log(data);
     
