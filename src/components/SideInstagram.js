a3b23b7f566bc8377b89fa88b5d95046c47c0677
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-16 11:36:54 +0100

    SideInstagram LG

--- src/components/SideInstagram.js
+++ src/components/SideInstagram.js
@@ -6,15 +6,15 @@ const SideInstagram = () => {
         <div className='mt-10'>
             <h3 className='font-semibold text-xl'><span className='py-0.5 px-2 text-white bg-green'>Instagram</span> Posts</h3>
             <div className='mt-10 flex flex-row flex-wrap gap-1 lg:gap-2'>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-instaDark rounded-lg flex justify-center items-center'><FaInstagram className='text-2xl text-white' /></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
-                <div className='w-[32%] aspect-square bg-insta rounded-lg'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-instaDark rounded-lg flex justify-center items-center lg:w-[30%]'><FaInstagram className='text-2xl text-white' /></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
+                <div className='w-[32%] aspect-square bg-insta rounded-lg lg:w-[30%]'></div>
             </div>
         </div>
     );
