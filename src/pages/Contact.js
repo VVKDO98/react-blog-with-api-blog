e63b7e9415c96be1c84394cc7bb788b68f7b59be
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-18 11:35:36 +0100

    Contact Responsive desktop

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,21 +6,21 @@ const Contact = () => {
         <div className='py-10 w-full'>
             <div className='w-4/5 mx-auto flex flex-col items-center justify-start'>
                 <h1 className='font-semibold text-xl'><span className='py-0.5 px-2 text-white bg-green'>Contact</span> Us</h1>
-                <div className='mt-10 w-full'>
-                    <div className='w-full'>
+                <div className='mt-10 w-full lg:flex lg:flex-row lg:items-start lg:gap-20'>
+                    <div className='w-full lg:w-1/2'>
                         <form action="" className='flex flex-col gap-6'>
-                            <div className='flex flex-col items-start gap-6'>
-                                <input className='w-full form-input px-4 py-3 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' type="text" name="name" placeholder='Name' />
-                                <input className='w-full form-input px-4 py-3 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' type="email" name="email" placeholder='Email' />
+                            <div className='flex flex-col items-start gap-6 lg:flex-row'>
+                                <input className='w-full form-input px-4 py-2 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' type="text" name="name" placeholder='Name' />
+                                <input className='w-full form-input px-4 py-2 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' type="email" name="email" placeholder='Email' />
                             </div>
                             <div className='flex flex-col gap-6'>
-                                <input className='w-full form-input px-4 py-3 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' type="text" name="subject" placeholder='Subject' />
-                                <textarea className='w-full px-4 py-3 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' name="message" placeholder='Message'></textarea>
+                                <input className='w-full form-input px-4 py-2 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal' type="text" name="subject" placeholder='Subject' />
+                                <textarea className='w-full px-4 py-2 rounded-md border border-1 border-form placeholder:text-bgcolor placeholder:text-sm placeholder:font-normal lg:h-40' name="message" placeholder='Type your message'></textarea>
                             </div>
-                            <input className='px-4 py-3 rounded-md bg-green text-white' type="button" value="Send message" />
+                            <input className='px-4 py-2 rounded-md bg-green text-white lg:w-32' type="button" value="Send message" />
                         </form>
                     </div>
-                    <div className='mt-10'>
+                    <div className='mt-10 lg:mt-0 lg:w-1/2'>
                         <p className='mb-5 text-base text-grey font-normal'>Dynamically underwhelm integrated outsourcing via timely models. Rapidiously reconceptualize visionary imperatives without</p>
                         <div className='mb-5 text-base text-grey font-normal flex flex-col'>
                             <a className='hover:text-green' href='mailto:blog.notebook@gmail.com'>blog.notebook@gmail.com</a>
@@ -28,12 +28,12 @@ const Contact = () => {
                             <p>9567 Turner Trace Apt. BC C3G8A4</p>
                         </div>
                         <div>
-                            <p className='text-lg font-semibold mb-2 text-black'>Follow On:</p>
+                            <p className='text-lg font-semibold mb-2 text-black'>Follow on :</p>
                             <div className='flex flex-row gap-5'>
-                                <a href="/"><FaFacebookF  className='text-2xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
-                                <a href="/"><FaTwitter  className='text-2xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
-                                <a href="/"><FaPinterest  className='text-2xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
-                                <a href="/"><FaInstagram  className='text-2xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
+                                <a href="/"><FaFacebookF  className='text-xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
+                                <a href="/"><FaTwitter  className='text-xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
+                                <a href="/"><FaPinterest  className='text-xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
+                                <a href="/"><FaInstagram  className='text-xl p-1 outline outline-1 rounded text-grey hover:text-white hover:bg-green' /></a>
                             </div>
                         </div>
                     </div>
