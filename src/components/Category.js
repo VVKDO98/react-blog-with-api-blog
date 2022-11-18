a22066ea8078d1ee0dee480a7caa0109ae82e886
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-18 16:00:54 +0100

    Update Category.js

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,16 +1,15 @@
-import React from 'react';
 import { useParams } from 'react-router-dom';
 import FetchCategoryByID from '../hooks/FetchCategoryByID'
 
 const Category = () => {
+    // Ne refresh pas au changement de page
     const {id} = useParams();
-    const value = {id}.id;
-    const {loading, error, data} = FetchCategoryByID(value);
+    const {loading, error, data} = FetchCategoryByID({id}.id);
 
     if (error) return <p>Loading of articles</p>;
     if (loading) return <div className="w-full h-screen flex items-center justify-center"><img src="./assets/img/spinner.svg" alt="" /></div>;
 
-    console.log(data[0].Posts);
+    // console.log(data[0].Posts);
     return (
         <div>
             <h1>Category {id}</h1>
