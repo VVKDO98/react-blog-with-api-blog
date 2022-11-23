8b5faa4aec2ef8eb943cec8ba5fcce968590e955
Doriano Vanvolcksom <vanvolcksom.doriano01@gmail.com>
2022-11-23 09:43:07 +0100

    Update Auth.js

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -25,27 +25,30 @@ export default function Auth() {
             console.log(data.session)
         }
     }
-    const Register = async (e) => {
-        e.preventDefault()
+    // const Register = async (e) => {
+    //     e.preventDefault()
 
-        const {data, error} = await supabase.auth.signUp({
-            email: email,
-            password: password,
-        })
-        if(error){
-            console.log(error.message)
-        }else{
-            setUser(data.user)
-        }
-    }
+    //     const {data, error} = await supabase.auth.signUp({
+    //         email: email,
+    //         password: password,
+    //     })
+    //     if(error){
+    //         console.log(error.message)
+    //     }else{
+    //         setUser(data.user)
+    //     }
+    // }
 
     return (
-        <div>
-            <h1>test</h1>
-            <input type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
-            <input type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-            <button onClick={signInWithEmail}>Login</button>
-            <button onClick={Register}>Register</button>
+        <div className='py-10 w-4/5 h-[600px] mx-auto flex flex-col justify-center items-center'>
+            <h1 className='mb-5 text-3xl font-semibold'>Login</h1>
+            <input className='mb-5 w-full h-10 bg-bannercolor rounded text-center text-grey lg:w-1/2' type="email" placeholder='Email' onChange={(e) => setEmail(e.target.value)}/>
+            <input className='mb-5 w-full h-10 bg-bannercolor rounded text-center text-grey lg:w-1/2' type="password" placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
+            <button className='w-full h-10 bg-green rounded text-center text-white font-semibold lg:w-1/2' onClick={signInWithEmail}>Login</button>
+            {/* <div className='w-full flex flex-col items-center justify-between'>
+                <button className='w-1/2 h-10 bg-bannercolor rounded text-center text-grey' onClick={Register}>Register</button>
+                <button className='w-full h-10 bg-green rounded text-center text-white font-semibold' onClick={signInWithEmail}>Login</button>
+            </div> */}
         </div>
     )
 }
